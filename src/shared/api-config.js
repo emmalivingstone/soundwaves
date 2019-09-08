0650509993d1aecdc31f6520fe8abd92762d31d0
Emma <emmalivingstone94@gmail.com>
2019-09-08 21:08:30 +0100

    Test

--- src/shared/api-config.js
+++ src/shared/api-config.js
@@ -1,5 +1,5 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 export const clientId = process.env.REACT_APP_CLIENT_ID;
-export const redirectUri = 'https://emmalivingstone.xyz';
+export const redirectUri = 'https://soundwaves.emmalivingstone.xyz/';
 export const scopes = ['user-top-read'];
