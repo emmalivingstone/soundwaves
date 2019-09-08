29acf7cf05fd796df8cfb230e206d2fb69aaa7d2
Emma <emmalivingstone94@gmail.com>
2019-09-08 20:55:15 +0100

    Redirecting to custom domain on successful login

--- src/shared/api-config.js
+++ src/shared/api-config.js
@@ -1,5 +1,6 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 export const clientId = process.env.REACT_APP_CLIENT_ID;
-export const redirectUri = 'http://localhost:3000/top-tracks/all-time';
+export const redirectUri =
+  'https://soundwaves.emmalivingstone.xyz/top-tracks/all-time';
 export const scopes = ['user-top-read'];
