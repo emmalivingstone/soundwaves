aa8976adfa07b4b386ca1dda5a53bd7c4033a9af
emmalivingstone <emmalivingstone94@gmail.com>
2019-09-08 10:48:56 +0100

    Adding Montserrat and Manjari google fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './scss/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+require('@openfonts/manjari_latin');
+require('typeface-montserrat');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
